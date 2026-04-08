afabb20e5df829e1c12ec88c1652f7c6de26cdc4
LuizSalvador-GIT <luizcesar.contisalvador@gmail.com>
2026-04-08 20:12:01 +0000

    primeiro codigo em c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main () [   
+    printf("teste funcionando em c /n");
+
+    int a =2;
+    int b =3;
+    int resultado = a + b;
+
+    printf("soma de %d +%d = %d/n a, b, resultado");
+
+    return 0
+]
